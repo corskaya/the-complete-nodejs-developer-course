0324a06b189ab415da06bd90ccd0728dd5712729
corskaya <cagriorskaya@hotmail.com>
2022-03-16 11:01:01 +0300

    import yargs, parse arguments

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,16 +1,70 @@
-// const getNotes = require('./notes.js')
-// console.log(getNotes())
-
-// Challenge: Use the chalk library in your project
-//
-// 1) Install version 2.4.1 of chalk
-// 2) Load chalk into app.js
-// 3) Use it to print the string "Success!" to the console in green
-// 4) Test your work
-//
-// Bonus: Use docs to mess around with other styles. Make text bold and inversed.
-
 const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
 
-console.log(chalk.green.bold.inverse('Success!'))
-console.log(chalk.bgMagenta('Text with bg of magenta'))
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Challenge 2: Add an option to yargs
+//
+// 1) Setup a body option for the add command
+// 2) Configure a description, make it required, and force it to be a string
+// 3) Log the body value in the handler function
+// 4) Test your work!
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true, // required or not
+      type: 'string'
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    console.log('Title: ' + argv.title)
+    console.log('Body: ' + argv.body)
+  }
+})
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing the note...')
+  }
+})
+
+// Challenge 1: Add two new commands
+//
+// 1) Setup command to suppert "list" command (print placeholder message for now)
+// 2) Setup command to support "read" command (print placeholder message for now)
+// 3) Test your work by running both commands and ensure correct output
+
+yargs.command({
+  command: 'list',
+  describe: 'List notes',
+  handler: function () {
+    console.log('Listing the notes...')
+  }
+})
+
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function () {
+    console.log('Reading the note...')
+  }
+})
+
+yargs.parse()
+
+// console.log(yargs.argv)
