bc32118ccb906dbb41e31d72a48fe4cf044e4e0c
corskaya <cagriorskaya@hotmail.com>
2022-03-17 20:29:09 +0300

    wire up address command option

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,17 +1,30 @@
 const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
-geocode('Istanbul', (error, data) => {
-  if (error) {
-    return console.log(error)
-  }
+// Challenge: Accept location via command line argument
+//
+// 1) Access the command line argument without yargs
+// 2) Use the string value as the input for geocode
+// 3) Only geocode if a location was provided
+// 4) Test your work with a couple locations
 
-  forecast(data.latitude, data.longitude, (error, forecastData) => {
+const address = process.argv[2]
+
+if (!address) {
+  console.log('No address provided')
+} else {
+  geocode(address, (error, data) => {
     if (error) {
       return console.log(error)
     }
 
-    console.log(data.location)
-    console.log(forecastData)
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error)
+      }
+
+      console.log(data.location)
+      console.log(forecastData)
+    })
   })
-})
+}
