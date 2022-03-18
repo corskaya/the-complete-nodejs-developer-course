9b351a4623db4905b36ca3d7dc31046f199d5484
corskaya <cagriorskaya@hotmail.com>
2022-03-18 23:50:12 +0300

    serve up css, js, images

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,20 +1,10 @@
 const express = require('express')
 const path = require('path')
 
-console.log(__dirname) // directory name
-console.log(path.join(__dirname, '../public'))
-
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
-
-// Challenge: Create two more HTML files
-//
-// 1) Create a html page for about with "About" title
-// 2) Create a html page for help with "Help" title
-// 3) Remove the old route handlers for both
-// 4) Visit both in the browser to test your work
 
 app.get('/weather', (req, res) => {
   res.send({
