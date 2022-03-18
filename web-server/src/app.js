15e1aeddfffda9e8a938b9f6453c9b42ef4d6b95
corskaya <cagriorskaya@hotmail.com>
2022-03-19 00:26:36 +0300

    add handlebars for templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,34 @@ const path = require('path')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs') // handlebars setup
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', { // string parameter matches with index.hbs inside views folder
+    title: 'Weather App',
+    name: 'Cagri Orskaya'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About App',
+    name: 'Cagri Orskaya'
+  })
+})
+
+// Challenge: Create a template for help page
+//
+// 1) Setup a help template to render a help message to the screen
+// 2) Setup the help route and render the template with an example message
+// 3) Visit the route in the browser and see your help message print
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'This is the help message'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
