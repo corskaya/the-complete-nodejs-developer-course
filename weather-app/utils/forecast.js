764080f8446908551ab5ab124bb5dac8b637a39d
corskaya <cagriorskaya@hotmail.com>
2022-03-18 08:50:41 +0300

    request with raw http module

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -4,7 +4,7 @@ const chalk = require('chalk')
 const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=41f301fcb12814c43c6c6ee56fb801f1&query=${latitude},${longitude}`
 
-  request({ url, json: true }, (error, { body }) => {
+  request({ url, json: true }, (error, { body } = {}) => {
     if (error) {
       callback(chalk.red('Unable to connect to weather service'), undefined)
     } else if (body.error) {
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -4,7 +4,7 @@ const chalk = require('chalk')
 const geocode = (address, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiY2FncmlvcnNrYXlhIiwiYSI6ImNsMHV3Z3h1dDAwcTYzY3FsZXdjbmk0Ym0ifQ.55iZsoB63UqMKPgUcLim1w&limit=1`
 
-  request({ url: url, json: true }, (error, { body }) => {
+  request({ url: url, json: true }, (error, { body } = {}) => {
     if (error) {
       const data = chalk.red('Unable to connect to location services')
       callback(data, undefined)
