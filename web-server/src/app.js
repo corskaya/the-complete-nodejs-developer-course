9e461ff765270521219bff0d416d17e0a2a075e0
corskaya <cagriorskaya@hotmail.com>
2022-03-18 15:10:30 +0300

    serve up html and json, fix latitude-longitude order

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,30 +2,33 @@ const express = require('express')
 
 const app = express()
 
-// app.com
-// app.com/help
-// app.com/about
-
 app.get('', (req, res) => {
-  res.send('Hello express!')
+  res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
-  res.send('Help page')
+  res.send({
+    name: 'Cagri',
+    age: 27
+  })
 })
 
-// Challenge: Setup two new routes
+// Challenge: Update routes
 //
-// 1) Setup an about route and render a page title
-// 2) Setup a weather route and render a page title
+// 1) Setup about route to render a title with HTML
+// 2) Setup a weather route to send back JSON
+//   - Object with forecast and location strings
 // 3) Test your work by visiting both in the browser
 
 app.get('/about', (req, res) => {
-  res.send('About page')
+  res.send('<h1>About</h1>')
 })
 
 app.get('/weather', (req, res) => {
-  res.send('Weather page')
+  res.send({
+    forecast: 'Sunny',
+    location: 'Istanbul'
+  })
 })
 
 app.listen(3000, () => {
