d4a37c9dde4999331b8ca41a29ee2daf4f2ad9c6
corskaya <cagriorskaya@hotmail.com>
2022-03-21 14:21:21 +0300

    fetch data from static weather url

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,20 @@
-console.log('Client side javascript file is loaded!')
+// Challenge: Fetch weather!
+//
+// 1) Setup a call to fetch to fetch weather for Boston
+// 2) Get the parse JSON response
+// - If error property, print error
+// - If no error property, print location and forecast
+// 3) Refresh the browser and test your work!
+
+const url = 'http://localhost:3000/weather?address=istanbul'
+
+fetch(url).then(response => {
+  response.json().then(data => {
+    if (data.error) {
+      console.log(data.error)
+    } else {
+      console.log(data.forecast)
+      console.log(data.location)
+    }
+  })
+})
