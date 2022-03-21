45a3c889e2286ea7d685ffbb46c13cd63d874660
corskaya <cagriorskaya@hotmail.com>
2022-03-21 14:54:10 +0300

    fetch dynamic data with search text

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,20 +1,24 @@
-// Challenge: Fetch weather!
+// Challenge: Use input value to get weather
 //
-// 1) Setup a call to fetch to fetch weather for Boston
-// 2) Get the parse JSON response
-// - If error property, print error
-// - If no error property, print location and forecast
-// 3) Refresh the browser and test your work!
+// 1) Migrate fetch call into the submit callback
+// 2) Use the search text as the address query string value
+// 3) Submit the form with a valid and invalid value to test
 
-const url = 'http://localhost:3000/weather?address=istanbul'
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
 
-fetch(url).then(response => {
-  response.json().then(data => {
-    if (data.error) {
-      console.log(data.error)
-    } else {
-      console.log(data.forecast)
-      console.log(data.location)
-    }
+weatherForm.addEventListener('submit', e => {
+  e.preventDefault()
+  const location = search.value
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        console.log(data.error)
+      } else {
+        console.log(data.forecast)
+        console.log(data.location)
+      }
+    })
   })
 })
