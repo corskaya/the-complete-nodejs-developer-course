72ed902a08f1ae1f283373dadfdfed0086f2d3f7
corskaya <cagriorskaya@hotmail.com>
2022-03-23 14:36:28 +0300

    insert data to database

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -11,5 +11,62 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     return console.log('Unable to connect to database!')
   }
 
-  console.log('Connect to database!')
+  const db = client.db(databaseName)
+
+  // db.collection('users').insertOne({
+  //   name: 'Cagri',
+  //   age: 23
+  // }, (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert user')
+  //   }
+
+  //   console.log(result.insertedId)
+  // })
+
+  // db.collection('users').insertMany([
+  //   {
+  //     name: 'Hakan',
+  //     age: 35
+  //   },
+  //   {
+  //     name: 'Hande',
+  //     age: 31
+  //   }
+  // ], (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert documents!')
+  //   }
+
+  //   console.log(result.insertedIds)
+  // })
+
+  // Challenge: Insert 3 tasks into a new tasks collection
+  //
+  // 1) Use insertMany to insert three documents
+  // - description (string), completed (boolean)
+  // 2) Setup the callback to handle error or print insertedIds
+  // 3) Run the script
+  // 4) Refresh the database in Robo 3t and view data in tasks collection
+
+  db.collection('tasks').insertMany([
+    {
+      description: 'task one',
+      completed: false
+    },
+    {
+      description: 'task two',
+      completed: true
+    },
+    {
+      description: 'task three',
+      completed: false
+    }
+  ], (error, result) => {
+    if (error) {
+      return console.log('Unable to insert tasks')
+    }
+
+    console.log(result.insertedIds)
+  })
 })
