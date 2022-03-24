34b5394953261de913b908b74d92d4e50a9dec16
corskaya <cagriorskaya@hotmail.com>
2022-03-24 10:55:37 +0300

    data validation

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,19 +1,59 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
+// Challenge 1: Add a password field to User
+//
+// 1) Setup the field as a required string
+// 2) Ensure the length is greater than 6
+// 3) Trim the password
+// 4) Ensure that password doesn't contain "password"
+// 5) Test your work
+
 const User = mongoose.model('User', {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid!')
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password cannot contain "password"!')
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number!')
+      }
+    }
   }
 })
 
 const me = new User({
-  name: 'Cagri',
-  age: 'mike'
+  name: '   Hakan  ',
+  email: 'HAKAN@gmail.com   ',
+  password: 'gfhdsjksf'
 })
 
 me.save().then(me => {
@@ -22,25 +62,26 @@ me.save().then(me => {
   console.log('Error', error)
 })
 
-// Challenge: Create a model for tasks
+// Challenge 2: Add validation and sanitization to task
 //
-// 1) Define the model with description and completed fields
-// 2) Create a new instance of the model
-// 3) Save the model to the database
-// 4) Test your work!
+// 1) Trim the description and make it required
+// 2) Make completed optional and default it to false
+// 3) Test your work with and without errors
 
 const Task = mongoose.model('Task', {
   description: {
-    type: String
+    type: String,
+    required: true,
+    trim: true,
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   }
 })
 
 const anyTask = new Task({
-  description: 'task one',
-  completed: true
+  description: 'task two'
 })
 
 anyTask.save().then(task => {
