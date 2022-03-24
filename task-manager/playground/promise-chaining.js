ea3103612de8ff59c5591b4e76447e6760e1c81a
corskaya <cagriorskaya@hotmail.com>
2022-03-24 17:31:19 +0300

    async-await

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,6 +1,8 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
+
+// Using promise chaining
 User.findByIdAndUpdate('623c5972c001eb4d7b90b938', { age: 1 }).then(user => {
   console.log(user)
   return User.countDocuments({ age: 1 })
@@ -10,10 +12,16 @@ User.findByIdAndUpdate('623c5972c001eb4d7b90b938', { age: 1 }).then(user => {
   console.log(e)
 })
 
-// Challenge: Mess around with promise chaining
-//
-// 1) Create promise-chaining-2.js
-// 2) Load in mongoose and task model
-// 3) Remove a given task by id
-// 4) Get and print the total number of incomplete tasks
-// 5) Test your work!
+// Using async/await
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age })
+  console.log(user)
+  const count = await User.countDocuments({ age })
+  return count
+}
+
+updateAgeAndCount('623c5972c001eb4d7b90b938', 2).then(count => {
+  console.log(count)
+}).catch(e => {
+  console.log(e)
+})
