f5aadc722e19c05571df695b8f9f56c16d66b6e9
corskaya <cagriorskaya@hotmail.com>
2022-03-28 02:28:57 +0300

    middleware functions

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,9 +6,33 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//   if (req.method === 'GET') {
+//     res.send('GET requests are disabled')
+//   } else {
+//     next()
+//   }
+// })
+
+// Challenge: Setup middleware for maintenance mode
+//
+// 1) Register a new middleware function
+// 2) Send back a maintenance message with a 503 status code
+// 3) Try your requests from the server and confirm status/message shows
+
+app.use((req, res, next) => {
+  res.status(503).send('Server is on maintenance mode')
+})
+
 app.use(express.json()) // automatically parse the incoming json data
 app.use(userRouter)
 app.use(taskRouter)
+
+//
+// Without middleware:  new request -> run route handler
+//
+// With middleware:     new request -> do something -> run route handler
+//
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`)
