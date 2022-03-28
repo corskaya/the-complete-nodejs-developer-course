0d0c353c88c828d38dfbd4664788305bed4903b7
corskaya <cagriorskaya@hotmail.com>
2022-03-28 15:55:40 +0300

    authenticate user update and delete endpoints

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -36,14 +36,6 @@ router.post('/users/logout', auth, async (req, res) => {
   }
 })
 
-// Challenge: Create a way to logout of all sessions
-//
-// 1) Setup POST /users/logoutAll
-// 2) Create the router handler to wipe the tokens array
-//    - Send 200 or 500
-// 3) Test your work
-//    - Login a few times and logout of all. Check database
-
 router.post('/users/logoutAll', auth, async (req, res) => {
   try {
     req.user.tokens = []
@@ -59,23 +51,14 @@ router.get('/users/me', auth, async (req, res) => {
   res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
-  const _id = req.params.id
+// Challenge: Refactor the update profile route
+//
+// 1) Update the URL to /users/me
+// 2) Add the authentication middleware into the mix
+// 3) Use the existing user document instead of fetching via param id
+// 4) Test your work in Postman!
 
-  try {
-    const user = await User.findById(_id)
-
-    if (!user) {
-      return res.status(404).send()
-    }
-
-    res.send(user)
-  } catch (e) {
-    res.status(500).send(e)
-  }
-})
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['name', 'email', 'password', 'age']
   const isValidOperation = updates.every(update => allowedUpdates.includes(update))
@@ -85,31 +68,21 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findById(req.params.id)
+    updates.forEach(update => req.user[update] = req.body[update])
+    await req.user.save()
 
-    updates.forEach(update => user[update] = req.body[update])
-
-    await user.save()
-
-    if (!user) {
-      return res.status(404).send()
-    }
-
-    res.send(user)
+    res.send(req.user)
   } catch (e) {
     res.status(400).send(e)
   }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id)
 
-    if (!user) {
-      return res.status(404).send()
-    }
+    await req.user.remove()
 
-    res.send(user)
+    res.send(req.user)
   } catch (e) {
     res.status(500).send()
   }
