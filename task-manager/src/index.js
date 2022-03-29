100565342d9bf88a856ee24ffc7088d754edc592
corskaya <cagriorskaya@hotmail.com>
2022-03-29 09:47:13 +0300

    set up relationship between user and task

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -25,3 +25,24 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`)
 })
+
+// PLAYGROUND
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+  // Finding User (creator) from task
+  const task = await Task.findById('6241b655fd6be3b6d16c0e8c')
+  console.log(task.owner) // prints -> new ObjectId("6241b57dde03f9a71c21d58b")
+  await task.populate('owner')
+  console.log(task.owner) // prints -> entire owner object with all public properties
+
+  // Finding Tasks from user
+  const user = await User.findById('6241b57dde03f9a71c21d58b')
+  console.log(user.tasks) // prints -> undefined
+  await user.populate('tasks')
+  console.log(user.tasks) // prints -> tasks array of associated user
+}
+
+main()
--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const Task = require('../models/task')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
-router.post('/tasks', async (req, res) => {
-  const task = new Task(req.body)
+router.post('/tasks', auth, async (req, res) => {
+  const task = new Task({
+    ...req.body,
+    owner: req.user._id
+  })
 
   try {
     await task.save()
@@ -22,11 +26,11 @@ router.get('/tasks', async (req, res) => {
   }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
   const _id = req.params.id
 
   try {
-    const task = await Task.findById(_id)
+    const task = await Task.findOne({ _id, owner: req.user._id })
 
     if (!task) {
       return res.status(400).send()
@@ -37,13 +41,6 @@ router.get('/tasks/:id', async (req, res) => {
     res.status(500).send(e)
   }
 })
-
-// Challenge: Change how tasks are updated
-//
-// 1) Find the task
-// 2) Alter the task properties
-// 3) Save the task
-// 4) Test your work by updating a task from Postman
 
 router.patch('/tasks/:id', async (req, res) => {
   const updates = Object.keys(req.body)
@@ -60,10 +57,6 @@ router.patch('/tasks/:id', async (req, res) => {
     updates.forEach(update => task[update] = req.body[update])
 
     await task.save()
-
-    // These 3 lines above do the same thing with Task.findByIdAndUpdate method
-
-    // const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
 
     if (!task) {
       return res.status(404).send()
