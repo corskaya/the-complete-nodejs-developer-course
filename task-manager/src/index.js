4f75bc2005194472b7eeb4a0019b869b42e45a57
corskaya <cagriorskaya@hotmail.com>
2022-03-30 16:09:52 +0300

    format uploading images

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,29 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// PLAYGROUND
-const multer = require('multer')
-const upload = multer({
-  dest: 'images',
-  limits: {
-    fileSize: 1000000
-  },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(doc|docx)$/)) {
-      return cb(new Error('Please upload a word document'))
-    }
-
-    cb(undefined, true)
-  }
-})
-
-// Multer's error handling structure
-app.post('/upload', upload.single('upload'), (req, res) => {
-  res.send()
-}, (error, req, res, next) => {
-  res.status(400).send({ error: error.message })
-})
-
 app.use(express.json()) // automatically parse the incoming json data
 app.use(userRouter)
 app.use(taskRouter)
