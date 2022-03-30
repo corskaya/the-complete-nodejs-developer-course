4080965502507fcb3ab51c8b1d2f3e881b26b6bf
corskaya <cagriorskaya@hotmail.com>
2022-03-30 10:20:28 +0300

    add support for file uploads

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const multer = require('multer')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -51,13 +52,6 @@ router.get('/users/me', auth, async (req, res) => {
   res.send(req.user)
 })
 
-// Challenge: Refactor the update profile route
-//
-// 1) Update the URL to /users/me
-// 2) Add the authentication middleware into the mix
-// 3) Use the existing user document instead of fetching via param id
-// 4) Test your work in Postman!
-
 router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['name', 'email', 'password', 'age']
@@ -86,6 +80,20 @@ router.delete('/users/me', auth, async (req, res) => {
   } catch (e) {
     res.status(500).send()
   }
+})
+
+// Challenge: Setup endpoint for avatar upload
+//
+// 1) Add POST /users/me/avatar to user router
+// 2) Setup multer to store uploads in an avatar directory
+// 3) Choose name "avatar" for the key when registering middleware
+// 4) Send back a 200 response from route handler
+// 5) Test your work. Create new Task App request and upload image
+
+const upload = multer({ dest: 'avatars/' })
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+  res.send()
 })
 
 module.exports = router
