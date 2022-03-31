72c935368cda765055141b40bffddb88c0f98a54
corskaya <cagriorskaya@hotmail.com>
2022-03-31 17:55:26 +0300

    refactor

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -14,14 +14,6 @@ const sendWelcomeEmail = (email, name) => {
   })
 }
 
-// Challenge: Send email to user on cancelation
-//
-// 1) Setup a new function for sending an email on cancelation
-//    - email and name as args
-// 2) Include their name in the email and ask why they canceled
-// 3) Call it just after the account is removed
-// 4) Run the request and check your inbox
-
 const sendCancelationEmail = (email, name) => {
   sgMail.send({
     to: email,
