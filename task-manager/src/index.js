f59a1606f720384e2f210e6c61bb24a0968e57d4
corskaya <cagriorskaya@hotmail.com>
2022-04-14 17:09:54 +0300

    add cors

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 require('./db/mongoose')
+const cors = require('cors')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT
 
+app.use(cors())
 app.use(express.json()) // automatically parse the incoming json data
 app.use(userRouter)
 app.use(taskRouter)
