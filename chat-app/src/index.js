b2b018fe07909853de8c519c61b2851f6451e5dc
corskaya <cagriorskaya@hotmail.com>
2022-07-26 17:23:27 +0300

    connect client and server

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,13 +1,18 @@
 const path = require("path");
 const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
-
 const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
+
 const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}!`);
+const io = socketio(server);
+io.on("connection", () => {
+  console.log("New WebSocket connection");
 });
