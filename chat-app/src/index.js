37112f89e250d6effc5a45cb7334889c3ff2a6da
corskaya <cagriorskaya@hotmail.com>
2022-07-27 09:56:47 +0300

    events (count example)

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -13,6 +13,18 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
 const io = socketio(server);
-io.on("connection", () => {
+
+let count = 0;
+
+io.on("connection", (socket) => {
   console.log("New WebSocket connection");
+
+  socket.emit("countUpdated", count);
+
+  socket.on("increment", () => {
+    count++;
+
+    // socket.emit("countUpdated", count);
+    io.emit("countUpdated", count);
+  });
 });
